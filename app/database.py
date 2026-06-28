24d5eee253f3b4be630d6fe776ba7f956ac46681
BilelSelmiIDnow <bilel.selmi@idnow.de>
2026-06-28 21:38:27 +0100

    Use PostgreSQL database URL in production

--- app/database.py
+++ app/database.py
@@ -1,11 +1,23 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./medical_assistant.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./medical_assistant.db"
+)
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {
+        "check_same_thread": False
+    }
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
